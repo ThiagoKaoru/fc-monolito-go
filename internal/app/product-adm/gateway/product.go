9e5acd63e11ce10e24d80a5131510401b43e7785
thiagokaoru <thiagotk987@gmail.com>
2024-06-18 01:09:33 -0300

    refactor: fix gateway implementation

--- internal/app/product-adm/gateway/product.go
+++ internal/app/product-adm/gateway/product.go
@@ -1,11 +1,11 @@
 package productGateway
 
 import (
-	"github.com/google/uuid"
 	productDomain "github.com/thiagokaoru/fc-monolito-go/internal/app/product-adm/domain"
+	IdValueObject "github.com/thiagokaoru/fc-monolito-go/internal/pkg/domain/value-object"
 )
 
 type ProductGateway interface {
-	Add(product productDomain.Product) error
-	Find(id uuid.UUID) (chan productDomain.Product, error)
+	Add(product *productDomain.Product) error
+	Find(id IdValueObject.ID) (*productDomain.Product, error)
 }
